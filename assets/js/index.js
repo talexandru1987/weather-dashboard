d0271d315d70a88281477f2e17d3bf367cbf5ffd
Alexandru <T_alexandru87@yahoo.com>
2022-05-08 20:11:08 +0100

    initial template

---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+const API_KEY = "YOUR_API_KEY";
+
+const renderCities = () => {
+  // get recent cities from LS []
+  // if [] is empty then render alert
+  // else render all recent cities
+  // add an event listener on div containing all cities
+};
+
+const renderCurrentWeather = (currentWeatherData) => {
+  // render the current weather data and append to section
+};
+
+const renderForecastWeather = (forecastWeatherData) => {
+  // render the forecast weather data and append each card to section
+};
+
+const renderWeatherData = (cityName) => {
+  // use API to fetch current weather data
+  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+
+  // from the response cherry pick all the data you want to see in the current weather card
+
+  // get the lat and lon from current weather data API response
+  const forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=metric&appid=${API_KEY}`;
+
+  // render current weather data
+
+  // render forecast weather data
+};
+
+const handleFormSubmit = () => {
+  // get the city name from input
+  // if city name is empty handle that
+  // else render weather data
+};
+
+const onReady = () => {
+  // render recent cities
+};
+
+$(document).ready(onReady);
