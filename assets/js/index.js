b63c3cd46be8ff442e669136d02a030cabae8d08
Alexandru <T_alexandru87@yahoo.com>
2022-05-12 18:26:36 +0100

    started the render cities function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,32 @@
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = "652867178169694693f42b9870238545";
+const recentCitiesContainer = $("#recent-cities");
+const currentAndFutureData = $("#render-data-container");
 
 const renderCities = () => {
   // get recent cities from LS []
+  const recentSearches = readLocalStorage("recentSearches", []);
   // if [] is empty then render alert
+  if (recentSearches.length === 0) {
+    //render alert
+    const alert = `<div class="recent-cities-alert btn"><p>No Past Searches!</p></div>`;
+  }
   // else render all recent cities
   // add an event listener on div containing all cities
+};
+
+//function to check ls and return values or default value
+const readLocalStorage = (lsObject, defaultV) => {
+  // get from using key name
+  const dataFromLS = localStorage.getItem(lsObject);
+
+  // parse data from LS
+  const parsedData = JSON.parse(dataFromLS);
+
+  if (parsedData) {
+    return parsedData;
+  } else {
+    return defaultV;
+  }
 };
 
 const renderCurrentWeather = (currentWeatherData) => {
@@ -37,6 +59,7 @@ const handleFormSubmit = () => {
 
 const onReady = () => {
   // render recent cities
+  renderCities();
 };
 
 $(document).ready(onReady);
